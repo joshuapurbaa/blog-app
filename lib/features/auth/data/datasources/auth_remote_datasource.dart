70b67978573deb06913b7f23525b001ebdf575ff
yoshuapurba <yoshuapurba27@gmail.com>
2024-03-11 09:15:22 +0700

    Implement sign in and sign up methods in AuthRemoteDataSourceImpl

--- lib/features/auth/data/datasources/auth_remote_datasource.dart
+++ lib/features/auth/data/datasources/auth_remote_datasource.dart
@@ -44,16 +44,68 @@ class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
   }
 
   @override
-  Future<UserModel> signInWithEmailAndPassword(
-      {required String email, required String password}) {
-    // TODO: implement signInWithEmailAndPassword
-    throw UnimplementedError();
+  Future<UserModel> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final response = await supabaseClient.auth.signInWithPassword(
+        password: password,
+        email: email,
+      );
+
+      if (response.user == null) {
+        throw ServerException(
+          message: 'An error occurred while signing in, user not found',
+        );
+      }
+
+      return UserModel.fromJson(
+        response.user!.toJson(),
+      );
+    } on AuthException catch (e) {
+      throw ServerException(
+        message: e.message,
+      );
+    } catch (e) {
+      throw ServerException(
+        message: e.toString(),
+      );
+    }
   }
 
   @override
-  Future<UserModel> signUpWithEmailAndPassword(
-      {required String name, required String email, required String password}) {
-    // TODO: implement signUpWithEmailAndPassword
-    throw UnimplementedError();
+  Future<UserModel> signUpWithEmailAndPassword({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final response = await supabaseClient.auth.signUp(
+        password: password,
+        email: email,
+        data: {
+          'full_name': name,
+        },
+      );
+
+      if (response.user == null) {
+        throw ServerException(
+          message: 'User not found',
+        );
+      }
+
+      return UserModel.fromJson(
+        response.user!.toJson(),
+      );
+    } on AuthException catch (e) {
+      throw ServerException(
+        message: e.message,
+      );
+    } catch (e) {
+      throw ServerException(
+        message: e.toString(),
+      );
+    }
   }
 }
