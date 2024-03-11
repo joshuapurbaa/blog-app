2789726129e42d85ffac4b348acb614e3e022932
yoshuapurba <yoshuapurba27@gmail.com>
2024-03-11 17:29:53 +0700

    Add init_dependencies.dart and init_dependencies.main.dart

---
+++ lib/init_dependencies.dart
@@ -0,0 +1,5 @@
+import 'package:blog_app/core/secrets/app_secrets.dart';
+import 'package:get_it/get_it.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+part 'init_dependencies.main.dart';
---
+++ lib/init_dependencies.main.dart
@@ -0,0 +1,14 @@
+part of 'init_dependencies.dart';
+
+final serviceLocator = GetIt.instance;
+
+Future<void> initDependencies() async {
+  final supabase = await Supabase.initialize(
+    url: AppSecrets.supabaseUrl,
+    anonKey: AppSecrets.supabaseAnonKey,
+  );
+
+  serviceLocator.registerLazySingleton(
+    () => supabase.client,
+  );
+}
