1f2dd2394832a09f49443170b6eb7fb6fc0d0aef
yoshuapurba <yoshuapurba27@gmail.com>
2024-03-11 17:29:41 +0700

    Add StaticString class and update AuthRepositoryImpl

---
+++ lib/core/constants/static_string.dart
@@ -0,0 +1,10 @@
+class StaticString {
+  static const List<String> topics = [
+    'Technology',
+    'Business',
+    'Programming',
+    'Entertainment',
+  ];
+
+  static const noConnectionMessage = 'No internet connection';
+}
--- lib/features/auth/data/repositories/auth_repository_impl.dart
+++ lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -1,29 +1,91 @@
 import 'package:blog_app/core/common/entities/user.dart';
+import 'package:blog_app/core/constants/static_string.dart';
+import 'package:blog_app/core/error/exception.dart';
+import 'package:blog_app/core/error/failure.dart';
+import 'package:blog_app/core/network/connection_checker.dart';
 import 'package:blog_app/features/auth/data/datasources/auth_remote_datasource.dart';
+import 'package:blog_app/features/auth/data/models/user_model.dart';
 import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:fpdart/fpdart.dart';
 
 class AuthRepoImpl implements AuthRepository {
   final AuthRemoteDataSource authRemoteDataSource;
+  final ConnectionChecker connectionChecker;
 
-  AuthRepoImpl(this.authRemoteDataSource);
+  AuthRepoImpl(this.authRemoteDataSource, this.connectionChecker);
 
   @override
-  AuthResult<User> currentUser() {
-    // TODO: implement currentUser
-    throw UnimplementedError();
+  AuthResult<User> currentUser() async {
+    try {
+      if (!await (connectionChecker.isConnected)) {
+        final session = authRemoteDataSource.currentUserSession;
+        if (session == null) {
+          return left(Failure('User not logged in'));
+        }
+
+        return right(
+          UserModel(
+            id: session.user.id,
+            email: session.user.email ?? '',
+            name: '',
+          ),
+        );
+      }
+
+      final user = await authRemoteDataSource.getCurrentUserData();
+
+      if (user == null) {
+        return left(Failure('User not logged in'));
+      }
+
+      return right(user);
+    } on ServerException catch (e) {
+      return left(Failure(e.message));
+    }
   }
 
   @override
-  AuthResult<User> signInWithEmailPassword(
-      {required String email, required String password}) {
-    // TODO: implement signInWithEmailPassword
-    throw UnimplementedError();
+  AuthResult<User> signInWithEmailPassword({
+    required String email,
+    required String password,
+  }) {
+    final result = _getUser(
+      () async => await authRemoteDataSource.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      ),
+    );
+    return result;
   }
 
   @override
-  AuthResult<User> signUpWithEmailPassword(
-      {required String name, required String email, required String password}) {
-    // TODO: implement signUpWithEmailPassword
-    throw UnimplementedError();
+  AuthResult<User> signUpWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  }) {
+    final result = _getUser(
+      () async => await authRemoteDataSource.signUpWithEmailAndPassword(
+        name: name,
+        email: email,
+        password: password,
+      ),
+    );
+    return result;
+  }
+
+  AuthResult<User> _getUser(Future<User> Function() fn) async {
+    try {
+      if (!await connectionChecker.isConnected) {
+        return left(
+          Failure(StaticString.noConnectionMessage),
+        );
+      }
+
+      final result = await fn();
+      return right(result);
+    } on ServerException catch (e) {
+      return left(Failure(e.message));
+    }
   }
 }
